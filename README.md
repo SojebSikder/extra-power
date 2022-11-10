c6cd5ee1a1715b71258c4f187ccd2272fe2ebe16
sojebsikder <sojebsikder@gmail.com>
2022-11-10 10:34:25 +0600

    main

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ This is a simple project that helps other developer to achieve something differe
 ## Convert html to pdf
 
 ```
-http://localhost:3000/converter/html2pdf?url=http://google.com
+https://extra-power.up.railway.app/converter/html2pdf?url=http://google.com
 ```
--- app/controllers/Pdf/PdfConverter/PdfConverter.controller.ts
+++ app/controllers/Pdf/PdfConverter/PdfConverter.controller.ts
@@ -11,31 +11,28 @@ export class PdfConverterController {
 
   @Get()
   async findAll(req: Request, res: Response) {
-    try {
-      const url = req.query.url.toString();
+    const url = req.query.url.toString();
 
-      const stream = res.writeHead(200, {
-        "Content-Type": "application/pdf",
+    const browser = await puppeteer.launch({
+      ignoreDefaultArgs: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+
+    await page.goto(url);
+    await page.emulateMediaType("screen");
+
+    const stream = res.writeHead(200, {
+      "Content-Type": "application/pdf",
+    });
+
+    (await page.createPDFStream())
+      .on("data", function (data) {
+        stream.write(data);
+      })
+      .on("end", async function () {
+        stream.end();
+        // Close the browser instance
+        await browser.close();
       });
-
-      const browser = await puppeteer.launch({
-        ignoreDefaultArgs: ["--no-sandbox"],
-      });
-      const page = await browser.newPage();
-
-      await page.goto(url);
-      await page.emulateMediaType("screen");
-      (await page.createPDFStream())
-        .on("data", function (data) {
-          stream.write(data);
-        })
-        .on("end", async function () {
-          stream.end();
-          // Close the browser instance
-          await browser.close();
-        });
-    } catch (error) {
-      res.send(error.message);
-    }
   }
 }
